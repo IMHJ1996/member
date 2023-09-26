49c6e5169ee752b28f91cd8e85e6ab3eb22a2e8a
IMHJ1996 <guswhdbear@naver.com>
2023-09-26 14:06:47 +0900

    수정

---
+++ src/main/java/com/icia/member/controller/MemberController.java
@@ -0,0 +1,103 @@
+package com.icia.member.controller;
+
+import com.icia.member.dto.MemberDTO;
+import com.icia.member.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "memberPages/memberSave";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        memberService.save(memberDTO);
+        return "memberPages/memberLogin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "memberPages/memberLogin";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        boolean loginResult = memberService.login(memberDTO);
+        if (loginResult) {
+            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+            return "memberPages/memberMain";
+        } else {
+            return "memberPages/memberLogin";
+        }
+    }
+
+    // /member
+    @GetMapping
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        model.addAttribute("memberList", memberDTOList);
+        return "memberPages/memberList";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        try {
+            MemberDTO memberDTO = memberService.findById(id);
+            model.addAttribute("member", memberDTO);
+            return "memberPages/memberDetail";
+        } catch (NoSuchElementException e) {
+            return "memberPages/NotFound";
+        } catch (Exception e) {
+            return "memberPages/NotFound";
+        }
+    }
+
+    @PostMapping("/dup-check")
+    public ResponseEntity emailCheck(@RequestBody MemberDTO memberDTO) {
+        boolean result = memberService.emailCheck(memberDTO.getMemberEmail());
+        if (result) {
+            return new ResponseEntity<>("사용가능", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("사용불가능", HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/axios/{id}")
+    public ResponseEntity detailAxios(@PathVariable("id") Long id) {
+        try {
+            MemberDTO memberDTO = memberService.findById(id);
+            return new ResponseEntity<>(memberDTO, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/update")
+    public String update(Model model, HttpSession session){
+        String memberEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.findByMemberEmail(memberEmail);
+        model.addAttribute("member",memberDTO);
+        return "memberPages/memberUpdate";
+    }
+    @PostMapping("/update")
+    public String update(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+        memberService.update(memberDTO);
+        session.removeAttribute("loginEmail");
+        return "/memberPages/memberLogin";
+    }
+}
